4936bdfe2f76ff3f78366cdc807cc689a0844ed9
崔灿 <aaa20052cc@163.com>
2019-07-09 23:32:00 +0800

    [cuican] refact

---
+++ cuican/LRU.js
@@ -0,0 +1,1 @@
+// last recently used
--- cuican/doubleLinkedList.js
+++ cuican/doubleLinkedList.js
@@ -3,55 +3,80 @@ class DoubleLinkedList {
         this.head = null
         this.size = 0
     }
+    addHead (value) {
+        if (this.size === 0 || !this.head) {
+            this.head = new Node(value)
+            this.size = 1
+            return
+        }
+        const newNode = new Node(value, null, this.head)
+        this.head.preNode = newNode
+        this.head = newNode
+        this.size += 1
+    }
+    addTail (value) {
+        if (this.size === 0 || !this.head) {
+            this.addHead(value)
+            return
+        }
+        const lastNode = this.getNode(this.size - 1)
+        lastNode.nextNode = new Node(value, lastNode, null)
+        this.size += 1
+    }
     add (index, value) {
         if (index < 0 || (!value && value !== 0)) {
             return false
         }
-        const currentSize = this.size
-        this.size += 1
-        if (currentSize === 0 || !this.head) {
-            this.head = new Node(value)
+        if (this.size === 0 || !this.head || index === 0) {
+            this.addHead(value)
             return true
         }
-        if (index === 0) {
-            const newNode = new Node(value, null, this.head)
-            this.head.preNode = newNode
-            this.head = newNode
-            return true
-        }
-        if (index >= currentSize) {
-            const currentNode = this.getNode(currentSize - 1)
-            currentNode.nextNode = new Node(value, currentNode, null)
+        if (index >= this.size) {
+            this.addTail(value)
             return true
         }
         const currentNode = this.getNode(index)
         const newNode = new Node(value, currentNode.preNode, currentNode)
         currentNode.preNode.nextNode = newNode
         currentNode.preNode = newNode
+        this.size += 1
         return true
+    }
+    removeHead () {
+        if (this.size === 0 || !this.head) {
+            return
+        }
+        if (this.head.nextNode) {
+            this.head.nextNode.preNode = null
+        }
+        this.head = this.head.nextNode
+        this.size -= 1
+    }
+    removeTail () {
+        if (this.size === 0 || !this.head) {
+            return
+        }
+        const currentNode = this.getNode(this.size - 1)
+        currentNode.preNode.nextNode = null
+        this.size -= 1
     }
     remove (index) {
         if (index < 0 || this.size === 0 || !this.head) {
             return false
         }
-        const currentSize = this.size
-        this.size -= 1
         if (index === 0) {
-            if (this.head.nextNode) {
-                this.head.nextNode.preNode = null
-            }
-            this.head = this.head.nextNode
+            this.removeHead()
             return true
         }
-        if (index >= currentSize) {
-            const currentNode = this.getNode(currentSize - 1)
-            currentNode.preNode.nextNode = null
+        if (index >= this.size) {
+            this.removeTail()
             return true
         }
         let currentNode = this.getNode(index)
         currentNode.preNode.nextNode = currentNode.nextNode
         currentNode.nextNode.preNode = currentNode.preNode
         currentNode = null
+        this.size -= 1
         return true
     }
     getNode (index) {
