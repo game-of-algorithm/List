9543e89b6ebf54a48d8ef6a876453d2e63481a79
HanFeng <hanfeng@guanghe.tv>
2019-07-10 00:41:44 +0800

    [feat][hanfeng] add LinkedList reversal method

--- hanfeng/LinkedList/LinkedList.test.js
+++ hanfeng/LinkedList/LinkedList.test.js
@@ -25,3 +25,39 @@ test('LinkedList add two node', () => {
   expect(testLinkedList.head.next.value).toBe(2)
   expect(testLinkedList.head.next.next).toBeNull()
 })
+
+test('reversal null linkedList', () => {
+  let testLinkedList = new LinkedList()
+
+  let reversalLinkedList = LinkedList.reversal(testLinkedList)
+
+  expect(reversalLinkedList.head).toBeNull()
+})
+
+test('reversal two node linkedList', () => {
+  let testLinkedList = new LinkedList()
+
+  testLinkedList.add(1)
+  testLinkedList.add(2)
+
+  let reversalLinkedList = LinkedList.reversal(testLinkedList)
+
+  expect(reversalLinkedList.head.value).toBe(2)
+  expect(reversalLinkedList.head.next.value).toBe(1)
+  expect(reversalLinkedList.head.next.next).toBeNull()
+})
+
+test('reversal three node linkedList', () => {
+  let testLinkedList = new LinkedList()
+
+  testLinkedList.add(1)
+  testLinkedList.add(2)
+  testLinkedList.add(3)
+
+  let reversalLinkedList = LinkedList.reversal(testLinkedList)
+
+  expect(reversalLinkedList.head.value).toBe(3)
+  expect(reversalLinkedList.head.next.value).toBe(2)
+  expect(reversalLinkedList.head.next.next.value).toBe(1)
+  expect(reversalLinkedList.head.next.next.next).toBeNull()
+})
