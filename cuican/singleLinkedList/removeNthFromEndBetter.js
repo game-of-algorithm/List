370ddab613142abe6b793df010bfad09e327f030
崔灿 <aaa20052cc@163.com>
2019-07-16 00:39:09 +0800

    cuican add better method

--- cuican/singleLinkedList/removeNthFromEndBetter.js
+++ cuican/singleLinkedList/removeNthFromEndBetter.js
@@ -1,3 +1,24 @@
 var removeNthFromEnd = function(head, n) {
-
+    if (!head) {
+        return head
+    }
+    if (!head.next) {
+        return null
+    }
+    let i = 0
+    let firstIndex = head
+    let secondIndex = head
+    while(i < n) {
+        firstIndex = firstIndex.next
+        i++
+    }
+    if (!firstIndex) {
+        return head.next
+    }
+    while (firstIndex.next) {
+        firstIndex = firstIndex.next
+        secondIndex = secondIndex.next
+    }
+    secondIndex.next = secondIndex.next.next
+    return head
 };
