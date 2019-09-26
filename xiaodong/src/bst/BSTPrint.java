a29620c86624e82118307bdd379d61a597972534
liuxiaodong <xiaodong@guanghe.tv>
2019-09-27 02:54:13 +0800

    长数字兼容

--- xiaodong/src/bst/BSTPrint.java
+++ xiaodong/src/bst/BSTPrint.java
@@ -5,6 +5,7 @@ import java.util.Queue;
 
 public class BSTPrint {
     public static void main(String[] args) {
+        int[] arr = new int[]{48, 46, 54, 97, 83, 69, 76, 25, 10, 5, 87, 12, 21, 99, 61, 33, 30, 47, 57, 4, 36, 42, 98, 66, 100, 17, 94, 81, 11, 77, 24, 89, 73, 53, 38, 7, 29, 8, 27, 23, 56, 70, 60, 85, 39, 65, 9, 75, 15, 67, 64, 22, 51, 82, 43, 3, 37, 91, 45, 13, 34, 63, 74, 71, 95, 55, 80, 92, 2, 19, 62, 40, 84, 41, 50, 88, 86, 59, 28, 44, 72, 68, 14, 35, 93, 26, 18, 78, 31, 58, 96, 6, 1, 90, 49, 16, 52, 79, 32, 20};
         BSTPrint bstPrint = new BSTPrint();
         Node node = bstPrint.getBST(7);
         bstPrint.printBST(node);
@@ -28,17 +29,24 @@ public class BSTPrint {
             levelNodeCount = queue.size();
             int startLeft = getStartLeft(bstLevel);
             int tempNodeIndex = 1;
-            printSpace(startLeft);
             while (tempNodeIndex <= levelNodeCount){
                 Node node = queue.poll();
                 if(node !=null && node.value != null){
                     if(tempNodeIndex > 1){
-                        printSpace((int) Math.pow(2,bstLevel+1)-1);
+                        printSpace((int) Math.pow(2,bstLevel+1)-1 -
+                                (node.value.toString().length()-1));//减去字体长度减1
+                    }else{
+                        printSpace(startLeft -(node.value.toString().length()-1));
                     }
                     System.out.print(node.value.toString());
                     queue.add(node.left);
                     queue.add(node.right);
                 }else{
+                    if(tempNodeIndex > 1){
+                        printSpace((int) Math.pow(2,bstLevel+1));
+                    }else{
+                        printSpace(startLeft +1);
+                    }
                     queue.add(null);
                     queue.add(null);
                 }
